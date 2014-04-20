026899c66c46529221187981c15f723208aa4741
Gabriel Velasco <gabriel.velasco001@gmail.com>
2014-04-20 10:42:08 -0400

    Added a public 2d array that holds the types that will be in each cell. This array makes drawing possible though its not a good idea.

---
+++ CustomTypes.h
@@ -0,0 +1,17 @@
+#ifndef CUSTOMTYPES_H_INCLUDED
+#define CUSTOMTYPES_H_INCLUDED
+
+#define TOTAL_ROWS 14
+#define TOTAL_COLUMNS 20
+#define BITMAP_DIMENSIONS 45
+
+typedef enum
+{
+    player_robot_blue_1,
+    player_robot_blue_2,
+    player_robot_blue_3,
+    player_robot_blue_4,
+    player_ground
+} Player_Type;
+
+#endif // CUSTOMTYPES_H_INCLUDED
